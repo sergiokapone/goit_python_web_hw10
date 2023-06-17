f95a452964463e3350972d584e183929b757f06d
sergiokapone <sergiy.ponomarenko@gmail.com>
2023-06-17 22:29:25 +0300

    add signup + signin

--- hw10/users/urls.py
+++ hw10/users/urls.py
@@ -5,4 +5,5 @@ app_name = "users"
 
 urlpatterns = [
     path("signup/", views.signupuser, name="signup"),
+    path("signin/", views.signinuser, name="signin"),
 ]
--- hw10/users/views.py
+++ hw10/users/views.py
@@ -9,22 +9,23 @@ from .register import RegisterForm
 
 def signupuser(request):
     if request.user.is_authenticated:
-        return redirect(to="quotesapp:main")
+        return redirect(to="quote:main")
 
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect(to="quotesapp:main")
+            print('Try redirect')
+            return redirect(to="quotes:root")
         else:
             return render(request, "users/signup.html", context={"form": form})
 
     return render(request, "users/signup.html", context={"form": RegisterForm()})
 
 
-def loginuser(request):
+def signinuser(request):
     if request.user.is_authenticated:
-        return redirect(to="quotes:main")
+        return redirect(to="quotes:root")
 
     if request.method == "POST":
         user = authenticate(
@@ -32,9 +33,9 @@ def loginuser(request):
         )
         if user is None:
             messages.error(request, "Username or password didn't match")
-            return redirect(to="users:login")
+            return redirect(to="users:signin")
 
         login(request, user)
-        return redirect(to="quotes:main")
+        return redirect(to="quotes:root")
 
     return render(request, "users/login.html", context={"form": LoginForm()})
